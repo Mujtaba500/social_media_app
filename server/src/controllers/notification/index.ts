1b5a90a9ab9506dc242f6738ac37b606ee3d5905
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-09-04 17:41:22 +0500

    Add get and clear notifications route handler

---
+++ server/src/controllers/notification/index.ts
@@ -0,0 +1,72 @@
+import { Response } from "express";
+import { customRequest, HttpStatusCode } from "../../types/types.js";
+import prisma from "../../db/config.js";
+
+const notificationController = {
+  getAll: async (req: customRequest, res: Response) => {
+    try {
+      const id = req.user?.userId;
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          notifications: true,
+        },
+      });
+
+      if (!user) {
+        return res.status(HttpStatusCode.UNAUTHORIZED).json({
+          message: "User does not exist",
+        });
+      }
+
+      res.status(HttpStatusCode.OK).json({
+        data: user.notifications,
+      });
+    } catch (err: any) {
+      console.log("Error while fetching notifications", err.message);
+      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+        message: "Internal server error",
+      });
+    }
+  },
+  deleteAll: async (req: customRequest, res: Response) => {
+    try {
+      const id = req.user?.userId;
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          notifications: true,
+        },
+      });
+
+      if (!user) {
+        return res.status(HttpStatusCode.UNAUTHORIZED).json({
+          message: "User does not exist",
+        });
+      }
+
+      await prisma.notification.deleteMany({
+        where: {
+          to: id,
+        },
+      });
+
+      res.status(HttpStatusCode.OK).json({
+        message: "Notifications deleted successfully",
+      });
+    } catch (err: any) {
+      console.log("Error while deleting notifications", err.message);
+      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+        message: "Internal server error",
+      });
+    }
+  },
+};
+
+export default notificationController;
--- server/src/routes/notification/index.ts
+++ server/src/routes/notification/index.ts
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import verifyToken from "../../middleware/auth/index.js";
+import notificationController from "../../controllers/notification/index.js";
 
 const notificationRouter = Router();
 
 // Get all notifications
-notificationRouter.get("/notifications", verifyToken);
+notificationRouter.get(
+  "/notifications",
+  verifyToken,
+  notificationController.getAll
+);
 
 // Clear all notifications
-notificationRouter.delete("/notifications", verifyToken);
+notificationRouter.delete(
+  "/notifications",
+  verifyToken,
+  notificationController.deleteAll
+);
 
 export default notificationRouter;
