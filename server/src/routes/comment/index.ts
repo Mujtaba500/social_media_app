5e96ebb139ac506e3f89b9a82b060facb5c77b4a
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-09-04 11:14:10 +0500

    Add comment validation

--- server/src/routes/comment/index.ts
+++ server/src/routes/comment/index.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import verifyToken from "../../middleware/auth/index.js";
+import commentValidator from "../../validators/comment/index.js";
 
 const commentRouter = Router();
 
-commentRouter.post("/comment/:postId", verifyToken);
+commentRouter.post("/comment/:postId", verifyToken, commentValidator);
 
 commentRouter.get("/comments/:postId", verifyToken);
 
-commentRouter.put("/comment/:commentId", verifyToken);
+commentRouter.put("/comment/:commentId", verifyToken, commentValidator);
 
 commentRouter.delete("/comment/:commentId", verifyToken);
 
