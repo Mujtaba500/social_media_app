a01adc5de5d82b41650c64a78b83020984f2a70f
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-09-04 11:34:03 +0500

    Add create comment route handler

---
+++ server/src/controllers/comment/index.ts
@@ -0,0 +1,57 @@
+import { Response } from "express";
+import { customRequest, HttpStatusCode } from "../../types/types.js";
+import prisma from "../../db/config.js";
+
+const commentController = {
+  createComment: async (req: customRequest, res: Response) => {
+    try {
+      const postId = req.params.postId;
+      const userId = req.user?.userId;
+      const body = req.body.body;
+
+      const post = await prisma.post.findUnique({
+        where: {
+          id: postId,
+        },
+      });
+
+      if (!post) {
+        return res.status(HttpStatusCode.NOT_FOUND).json({
+          message: "Post not found",
+        });
+      }
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
+
+      if (!user) {
+        return res.status(HttpStatusCode.UNAUTHORIZED).json({
+          message: "Unauthorized",
+        });
+      }
+
+      const comment = await prisma.comment.create({
+        data: {
+          body,
+          postId: postId,
+          authorId: userId!,
+        },
+      });
+
+      res.status(HttpStatusCode.OK).json({
+        message: "Commented sucessfully",
+        data: comment,
+      });
+    } catch (err: any) {
+      console.log("Error while creating comment", err.message);
+      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+        message: "Internal server error",
+      });
+    }
+  },
+};
+
+export default commentController;
--- server/src/routes/comment/index.ts
+++ server/src/routes/comment/index.ts
@@ -1,17 +1,28 @@
 import { Router } from "express";
 import verifyToken from "../../middleware/auth/index.js";
 import commentValidator from "../../validators/comment/index.js";
+import commentController from "../../controllers/comment/index.js";
 
 const commentRouter = Router();
 
-commentRouter.post("/comment/:postId", verifyToken, commentValidator);
+// Create comment
+commentRouter.post(
+  "/comment/:postId",
+  verifyToken,
+  commentValidator,
+  commentController.createComment
+);
 
+// Get comments for a single post
 commentRouter.get("/comments/:postId", verifyToken);
 
+// Edit comment
 commentRouter.put("/comment/:commentId", verifyToken, commentValidator);
 
+// delete comment
 commentRouter.delete("/comment/:commentId", verifyToken);
 
+// Like/ unlike comment
 commentRouter.put("/comment/like/:commentId", verifyToken);
 
 export default commentRouter;
